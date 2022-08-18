0c21691846f3f07eef807d961e51235ec072cc36
etotmenina <totmeninal@mail.ru>
2022-08-18 14:08:46 +0800

    Draft MaxPool

--- test/test_math.hpp
+++ test/test_math.hpp
@@ -123,6 +123,143 @@ LTESTexe(exp_f32, {
     for (size_t i = 0; i < src.size(); i++ )
         EXPECT_NEAR((float)(dest[i]), (float)(::exp(src[i])), 1.e-39f);
     });
+
+template<class T>
+void printData(T* data, int H, int W, int C) {
+    for (int i = 0; i < C; i++)
+    {
+        for (int j = 0; j < H * W; j++) {
+            std::cout << data[i*H*W+j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+void MaxPool(float* data, int H, int W, int C, float* result, int H0, int W0, int kh, int kw){
+    for (int c = 0; c < C; c++)
+    {
+        for (int y = 0; y < H0; y++) {
+            for (int x = 0; x < W0; x++) {
+                float tmp = data[(c*H*W)+(y*W+x)];
+                for (int hi = 0; hi < kh; hi++)
+                    for (int hj = 0; hj < kw; hj++)
+                    {
+                        tmp = std::max(tmp, data[(c*H*W)+(y*W+x)+(hi*W+hj)]);
+                    }
+                
+                result[c*H0*W0+y*W0+x] = tmp;
+            }
+        }
+    }
+}
+
+PTEST_2(max_pool, int64_t, kh, int64_t, kw, {
+    IReg data, H, W, C, result, H0, W0;
+    USE_CONTEXT_(CTX);
+    STARTFUNC_(TESTNAME, &data, &H, &W, &C, &result, &H0, &W0)
+    {
+        getImpl(getImpl(&CTX)->getCurrentFunc())->overrideRegisterSet(RB_INT, { 0, 1, 2, 3, 4, 5, 6, 7 }, { 0, 1, 2, 3, 4, 5, 6, 7 }, {}, { 18, 19, 20, 21, 22 });
+        IReg offset = CONST_(0);
+        IReg c = CONST_(0);
+        IReg check = W0 * sizeof(float);
+        // std::vector<std::vector<IReg> > offstab(kh, std::vector<IReg>(kw, IReg()));
+        // for(int i = 0; i < kh; i++ ) 
+        //     for(int j = 0; j < kh; j++)
+        //         offstab[i][j] = IReg((CONST_(i)*W+CONST_(j)) * sizeof(float);  
+        IReg offstab_0_0 = (0*W+0)* sizeof(float); 
+        IReg offstab_0_1 = (0*W+1)* sizeof(float); 
+        IReg offstab_0_2 = (0*W+2)* sizeof(float); 
+        IReg offstab_1_0 = (1*W+0)* sizeof(float); 
+        IReg offstab_1_1 = (1*W+1)* sizeof(float); 
+        IReg offstab_1_2 = (1*W+2)* sizeof(float); 
+        IReg offstab_2_0 = (2*W+0)* sizeof(float); 
+        IReg offstab_2_1 = (2*W+1)* sizeof(float); 
+        IReg offstab_2_2 = (2*W+2)* sizeof(float); 
+        WHILE_(c < C)
+        {
+            IReg y = CONST_(0);
+            WHILE_(y < H0) {
+                IReg x = CONST_(0);
+                IReg next_x = x + CTX.vbytes();
+                // IReg next_data += CTX.vbytes();
+                WHILE_(x < check) {
+                    IReg border = W * sizeof(float);
+                    IF_(next_x > border){
+                        x = x - (next_x - border);
+                        data = data - (next_x - border);
+                    }
+
+                    IReg base_offset = (c*H*W* sizeof(float))+(y*W* sizeof(float)+x);
+                    VReg<float> tmp1 = max(loadvec<float>(data, offstab_0_0 ),
+                                           loadvec<float>(data, offstab_0_1 ));
+                    VReg<float> tmp2 = max(tmp1, loadvec<float>(data, offstab_0_2));
+                    VReg<float> tmp3 = max(tmp2, loadvec<float>(data, offstab_1_0));
+                    VReg<float> tmp4 = max(tmp3, loadvec<float>(data, offstab_1_1));
+                    VReg<float> tmp5 = max(tmp4, loadvec<float>(data, offstab_1_2));
+                    VReg<float> tmp6 = max(tmp5, loadvec<float>(data, offstab_2_0));
+                    VReg<float> tmp7 = max(tmp6, loadvec<float>(data, offstab_2_1));
+                    VReg<float> tmp8 = max(tmp7, loadvec<float>(data, offstab_2_2));
+
+                    storevec(result, (c*H0*W0* sizeof(float)+y*W0* sizeof(float)+x), tmp8);
+                    x = x + CTX.vbytes();
+                    data += CTX.vbytes();
+                }
+                y = y + 1;
+                // data += sizeof(float) * W;
+            }
+            c = c + 1;
+            // data += sizeof(float) * W * H;
+        }
+        RETURN_();
+    }
+    });
+
+
+PTESTexe_2(max_pool, int64_t, kh, int64_t, kw, {
+    typedef void (*max_pool_f)(float* data, int H, int W, int C, float* result, int H0, int W0);
+    max_pool_f tested = reinterpret_cast<max_pool_f>(EXEPTR);
+
+    int W = 9;
+    int H = 9;
+    int C = 3;
+    int H_W = W*H;
+    int H0 = H-kh+1;
+    int W0 = W-kw+1;
+
+    std::vector<float> storage(W*H*C, 0);
+    float* c = &(storage[0]);
+
+    for (int i = 0 ; i < C ; i++) {
+        float* HW = c + i * H_W;
+        for (int j = 0; j < H_W; j++) {
+            HW[j] = rand() % 1000;
+        }
+    }
+    printData(c, H, W, C);
+
+    std::vector<float> result(H0*W0*C, 0);
+    float* c_out = &(result[0]);
+
+    MaxPool(c, H, W, C, c_out, H0, W0, kh, kw);
+    printData(c_out, H0, W0, C);
+
+    std::vector<float> result_vec(H0*W0*C, 0);
+    float* c_out_vec = &(result_vec[0]);
+
+    tested(c, H, W, C, c_out_vec, H0, W0);
+    printData(c_out_vec, H0, W0, C);
+
+    for(int channel = 0; channel < C; channel++) 
+        for(int row = 0; row<H0; row++) 
+            for(int col = 0; col<H0 ; col++)
+                // if(c_out[channel* W0 * H0 + row*W0 + col] != c_out_vec[channel* W0 * H0 + row*W0 + col])
+                //     std::cout<<"(Channel:"<< channel << ", Row:" << row << ", Col:" << col <<")|" << c_out[channel* W0 * H0 + row*W0 + col]<<"!="<<c_out_vec[channel* W0 * H0 + row*W0 + col]<<std::endl;
+                EXPECT_EQ(c_out[channel* W0 * H0 + row*W0 + col], c_out_vec[channel* W0 * H0 + row*W0 + col]);
+
+    });
+
+PTESTfix_2(max_pool, 3, 3);
+
 #endif //__LOOPS_ARCH == __LOOPS_AARCH64
 
 };
