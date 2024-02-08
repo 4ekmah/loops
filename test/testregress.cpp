49f4a68e932b897a3223f24d4bcd3e32ccaad479
4ekmaH <25587680+4ekmah@users.noreply.github.com>
2024-02-08 12:33:32 +0300

    Miniz is added, some simplest operations are tested.

--- test/testregress.cpp
+++ test/testregress.cpp
@@ -11,10 +11,23 @@ See https://github.com/4ekmah/loops/LICENSE
 #include "testregress/test_aarch64.hpp"
 #include "testregress/test_intel64.hpp"
 #include <gtest/gtest.h>
+#include <zip_file.hpp>
 
-//DUBUG: don't forget to append license information.
 int main(int argc, char** argv)
 {
+    // Write example
+    // miniz_cpp::zip_file file;
+    // file.write("/home/qohen/development/linux/loops/test/refasm/aarch64/linux/a_plus_b.tst", "meow/a_plus_b.tst");
+    // file.save("/home/qohen/development/linux/loops/test/refasm/aarch64_linux.zip");
+
+    // Read example
+    // miniz_cpp::zip_file file("/home/qohen/development/linux/loops/test/refasm/aarch64_linux.zip");
+    // // auto meow = file.infolist();
+    // // file.extract(meow[0], "/home/qohen/development/linux/loops/test/refasm");
+    // // Subdirectory meow have to be precreated, miniz doesn't care about it and fails to create new file in given path.
+    // file.extract("meow/a_plus_b.tst", "/home/qohen/development/linux/loops/test/refasm");
+    // return 0;
+        
     ::testing::InitGoogleTest(&argc, argv);
 
     return RUN_ALL_TESTS();
